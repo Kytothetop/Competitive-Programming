9c40f9bb7f3fdbe3276127e3798a2f15174e89c3
Kytothetop <87958344+Kytothetop@users.noreply.github.com>
2024-04-16 22:59:22 +0700

    Example
    I created example tttttt

---
+++ Others/Example.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Bing Chilling CHINA";
+    return 0;
+}
--- Others/Fedbxy Grader/P-004 Emirp/main.cpp
+++ Others/Fedbxy Grader/P-004 Emirp/main.cpp
@@ -1,53 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-//bool solve()
-//{
-//    // 6m
-//    // 6m -1 , 6m +1 return 1
-//
-//    // 2m
-//    // 3m
-//    // 4m
-//    // return 0
-//
-//    // 6m -1 = n
-//    // 6m = n+1
-//    long long int n;
-//    cin >> n;
-//    if (n <= 3)
-//        return n == 2 || n == 3;
-//    else for(long long int i = 6; i <= n/i ; i+= 6)
-//            {
-//                if((n%(i+1) == 0) or (n%(i-1) ==0))
-//                    return 0;
-//            }
-//
-//    return 1;
-//}
-bool solve()
-{
-    unsigned long int n;
-    cin >> n;
-        if (n <= 3)
-        return (n == 2 || n == 3);
-        if (n%2 == 0 or n%3 == 0)return 0;
-            for (unsigned int i =3; i<=n/i ; i+=2 )
-                {
-                    if(n%i==0)
-                        return 0;
-                }
-            return 1;
-}
+
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    short t;
-    cin >> t;
-    while(t--)
-        {
-            cout << (solve() ?  "Yes" : "No");
-            cout << '\n';
-        }
+    cout << "Bruh";
 }
